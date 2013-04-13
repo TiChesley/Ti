0a951aeac71874c99e170be8f166f2c548bdcf35
Ti Chesley <mattiaschesley@gmail.com>
2013-04-13 18:58:12 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ti::Application.config.secret_token = 'f36d345fe0cbff40ec669f8b0283dfaa6f47fed4d3c55170e7090126b29b0cf7f44bc064e264c2a86bbc62848148b9eee344fb9561334512547397fcc9447532'
