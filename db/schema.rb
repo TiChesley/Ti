cf57804e505c97c4f0a629ca1e587d1f9c54f731
Ti Chesley <mattiaschesley@gmail.com>
2013-04-30 13:04:18 -0400

    Add Image Uploading via URL

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130426202049) do
+ActiveRecord::Schema.define(:version => 20130430164847) do
 
   create_table "pins", :force => true do |t|
     t.string   "description"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(:version => 20130426202049) do
     t.string   "image_content_type"
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
+    t.string   "image_remote_url"
   end
 
   create_table "users", :force => true do |t|
